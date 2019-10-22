cc8afb6c2790b80248d1f444ac69cbf4ccfc77e3
andermm <andersonmaliszewski@gmail.com>
2019-10-22 19:27:14 -0300

    npb exec script

--- nas_exe.sh
+++ nas_exe.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
 NPB=$HOME/CMP223_P4/NPB3.4-MPI/bin
+P4=$HOME/CMP223_P4
 START=`date +"%d-%m-%Y.%Hh%Mm%Ss"`
-LOGS=$HOME/CMP223_P4/logs
+LOGS=$P4/logs_nas
 OUTPUT=$LOGS/npb.$START.csv
+PROJECT=$P4/lib/experimental_project.csv
+MACHINEFILE=$P4/lib/nas_nodes
 
-echo output file: $OUTPUT
 
-echo "env,app,class,metric,value" > $OUTPUT
+#Read the experimental project
+tail -n +2 $PROJECT |
+	while IFS=, read -r name runnoinstdorder runno runnostdrp \
+		kernels inputs Blocks
+do
+	#Clean the values
+	export name=$(echo $name | sed "s/\"//g")
+	export kernels=$(echo $kernels | sed "s/\"//g")
+	export inputs=$(echo $inputs | sed "s/\"//g")
 
-cd $NPB
+	# Define a single key
+	KEY="$name-$kernels-$inputs"
+	
+	echo $KEY
 
-for step in `seq 1 30`; do
-	echo "step: $step"	
-	for app in *.x; do
-		printf "\t app: $app \n"
-			mpirun \
-				-np 4 \
-				-machinefile nas_nodes 
-				$app 2> $LOGS/nas.err 1> $LOGS/nas.time
-			TIME=`grep -i "Time in seconds" $LOGS/nas.time | awk {'print $5'}`
-			echo "native,${app:0:2},${app:3:1},time,$TIME" >> $OUTPUT
-		done
-	done
-echo "done"
+	# Prepare the command for execution
+	runline=""
+	runline+="mpirun -np 4 "
+
+	runline+=" -machinefile $MACHINEFILE "
+	runline+="$NPB/$kernels$inputs "
+	runline+="2>> $LOGS/nas.err"
+	runline+=" &> >(tee -a $LOGS/backup/$kernels${inputs:0:1}.log > $LOGS/nas.out)"
+	
+	
+	# 3.3 Executes the experiments
+	echo "Running >> $runline <<"
+	eval "$runline < /dev/null"
+	
+	#cp $LOGS/nas.time $LOGS/nas.backup
+
+	TIME=`grep -i "Time in seconds" $LOGS/nas.out | awk {'print $5'}`
+	echo "${kernels:0:2},${inputs:0:1},$TIME" >> $OUTPUT
+	echo "Done!"
+done
+exit
