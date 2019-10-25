7333416d038f0027e368df46310415eee61883f4
andermm <andersonmaliszewski@gmail.com>
2019-10-24 22:47:41 -0300

    R updated

--- R/DoE.R
+++ R/DoE.R
@@ -1,15 +1,13 @@
-
-library(DoE.base)
-set.seed(0)
-wscad <- fac.design(factor.names = list(
-  kernels = c("bt.", "ep.", "cg.", "mg.", "lu.", "sp."),
-  inputs = c("C.x", "D.x")),
-  replications=30, 
-  randomize=TRUE)
-  print(wscad)
-
-export.design(wscad,
-              filename = "projeto-experimental",
-              type = "csv",
-              replace = TRUE
-)
+  library(DoE.base)
+  set.seed(0)
+  wscad <- fac.design(factor.names = list(
+    kernels = c("bt.", "ep.", "cg.", "mg.", "lu.", "sp.", "is.", "ft."),
+    inputs = c("C.x", "D.x"),
+    btl = c("^tcp", "tcp,self")),
+    replications=30,
+    randomize=TRUE)
+    print(wscad)
+  
+  write.table(wscad, file = "experimental_project.csv",
+                sep=";",
+  )
