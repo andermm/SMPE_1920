fed8134f515e4f55b07e57f8021563647b800b6d
Anderson <andersonmaliszewski@gmail.com>
2019-10-29 13:08:41 -0300

    updated

--- R/DoE.R
+++ R/DoE.R
@@ -1,9 +1,8 @@
   library(DoE.base)
   set.seed(0)
   wscad <- fac.design(factor.names = list(
-    kernels = c("bt.", "ep.", "cg.", "mg.", "lu.", "sp.", "is.", "ft."),
-    inputs = c("C.x", "D.x"),
-    btl = c("^tcp", "tcp,self")),
+    kernels = c("bt.D.x", "ep.D.x", "cg.D.x", "mg.D.x", "lu.D.x", "sp.D.x", "is.D.x", "ft.D.x"),
+    btl = c("tcp --mca btl_tcp_if_include ib0", "tcp --mca btl_tcp_if_include eno2", "openib")),
     replications=30,
     randomize=TRUE)
     print(wscad)
