24a95c6306e8473b7af605b872594c7c9d0a9683
andermm <andersonmaliszewski@gmail.com>
2019-11-07 22:33:49 -0300

    ok

--- R/DoE.R
+++ R/DoE.R
@@ -1,6 +1,6 @@
   library(DoE.base)
   set.seed(0)
-  wscad <- fac.design(factor.names = list(
+  smpe <- fac.design(factor.names = list(
     kernels = c("bt.D.x", "ep.D.x", "cg.D.x", "mg.D.x", "lu.D.x", "sp.D.x", "is.D.x", "ft.D.x"),
     btl = c("tcp --mca btl_tcp_if_include ib0", "tcp --mca btl_tcp_if_include eno2", "openib")),
     replications=30,
