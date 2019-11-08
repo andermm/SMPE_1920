b41dfe60364b6f270bdba309785f408400d4d8ee
andermm <andersonmaliszewski@gmail.com>
2019-11-08 19:23:38 -0300

    DoE updated

--- R/DoE.R
+++ R/DoE.R
@@ -2,7 +2,7 @@
   set.seed(0)
   smpe <- fac.design(factor.names = list(
     apps = c("bt.D.x", "ep.D.x", "cg.D.x", "mg.D.x", "lu.D.x", "sp.D.x", "is.D.x", "ft.D.x"),
-    interface = c("eth", "ib", "openib")),
+    interface = c("eth", "ib", "ipoib")),
     replications=30,
     randomize=TRUE)
     print(smpe)
