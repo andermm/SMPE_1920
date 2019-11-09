a9ba6a5ee9a457274ac0dc99f21d60ebb1d8fd06
andermm <andersonmaliszewski@gmail.com>
2019-11-08 21:45:30 -0300

    update

--- BATCH/central.sh
+++ BATCH/central.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 salloc -p hype --exclusive --nodelist=hype1,hype2,hype3,hype4 -J JOB -t 72:00:00
 
-ssh hype1 '$HOME/SMPE_1920/BATCH/./nas.batch'
+ssh -n -f hype1 "sh -c 'cd $HOME/SMPE_1920/BATCH; nohup ./nas.batch > /dev/null 2>&1 &'"
